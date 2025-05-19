416704b4552ab45276408a5589d8dbdc2e5450fe
Null <86795298+Nu11ified@users.noreply.github.com>
2025-05-19 02:29:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     eslint: {
-        ignoreDuringBuilds: true,
-      },
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;

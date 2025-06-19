0eaa52185bc7660d9c7b9d2ebb238b48a0e58c3f
Null <86795298+Nu11ified@users.noreply.github.com>
2025-06-18 22:56:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ const config = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-          { key: 'Content-Security-Policy', value: "default-src 'self';" },
+          { key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' data: https:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self';" },
         ],
       },
     ];
